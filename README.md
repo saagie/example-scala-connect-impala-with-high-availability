0b6ae9b43d07243bed11c633c8d03b42e223be5b
Qiwei He <qiwei.he@saagie.com>
2020-02-20 11:57:36 +0100

    Refactor of code

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Connect to Impala with High availability
 
 An example to show how to connect to Impala with a random active datanode.
 
-
 ## Usage in Saagie
 
 - sbt assembly
@@ -12,3 +11,28 @@ An example to show how to connect to Impala with a random active datanode.
 - Add impala user, impala password and the database name in arguments in the command line (The user have to had ALL privilege in the database)
 - Use default parameters 
 - Create and Launch
+
+## Usage in another SBT project
+
+- Add the folowing code in your `build.sbt` file of your sbt project
+```
+lazy val gitRepo = "git:https://github.com/saagie/example-scala-connect-impala-with-high-availability.git"
+lazy val g = RootProject(uri(gitRepo))
+lazy val root = project in file(".") dependsOn g
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+```
+
+- Check if you have already a `plugin.sbt` file in `project/plugins.sbt` in order to use sbt assembly
+- Then, in your class, you can use the function `ImpalaHA.get_all_active_datanode` in order to get a list of available datanodes, with the following parameter
+    - list_datanodes: `Seq[String]` List of Impala Datanodesn eg: `Seq("dn1","dn2","dn3","dn4")` :rotating_light: in this example, we have four datanodes, you have to change it according to your situation.
+    - user: `String` User that we can use to connect to the database 
+    - pwd: `String` Password of the user 
+    - databaseName: `String` Name of database that user can access eg: `default`
+- Or, you can use the function `ImpalaHA.random_node_connect` in order to set-up a connection to a random available datanode with the same parameters.
+ 
+### Environment variables used
+- `PORT_IMPALA`: default value set as: `21050`
+- `LIST_DATANODES`
--- src/main/scala/ImpalaHA.scala
+++ src/main/scala/ImpalaHA.scala
@@ -1,16 +1,17 @@
 import java.sql.{Connection, DriverManager}
+
 import org.slf4j.{Logger, LoggerFactory}
-import scala.util.Random
+
+import scala.util.{Failure, Random, Success, Try}
 import scala.collection.mutable.ListBuffer
 
 object ImpalaHA {
   val logger: Logger = LoggerFactory.getLogger(getClass)
-
   def main(args: Array[String]):Unit = {
     val dns =  sys.env.getOrElse("LIST_DATANODES", "dn1;dn2;dn3;dn4;dn5;dn6;dn7;dn8;dn9" )
     val dn_list = dns.split(";")
-    if(args.size < 3){
-      logger.warn("3 arg is required:\n \t- user, pwd, database ")
+    if(args.length < 3){
+      logger.warn("Three arg are required:\n \t- user, pwd, database ")
     }
     val user = args(0)
     val pwd = args(1)
@@ -22,13 +23,19 @@ object ImpalaHA {
     val con = random_node_connect(dn_list,user, pwd, databaseName)
     val stmt = con.createStatement()
     val sqlStatementShow = "SHOW TABLES"
-    stmt.execute(sqlStatementShow)
+    val resultSet = stmt.executeQuery(sqlStatementShow)
+    while ( resultSet.next() ) {
+      val tableName = resultSet.getString("name")
+      logger.info("Table name: " + tableName)
+    }
+    stmt.close()
+    con.close()
 
   }
 
   /**
    * Return a list of available dn
-   * @param list_datanodes List of Impala URLs
+   * @param list_datanodes List of Impala Datanodes
    * @param user          user that we can use to connect to the database
    * @param pwd           password of the user
    * @param databaseName  Name of database that user can access
@@ -44,6 +51,7 @@ object ImpalaHA {
         // Test if the connection is working
         val con = DriverManager.getConnection(connectionURL, user, pwd)
         list_active_name_node += dn
+        con.close()
       }
       catch {
         case e: Throwable =>
@@ -68,12 +76,30 @@ object ImpalaHA {
    * @param databaseName  Name of database that user can access
    */
   def random_node_connect(list_datanodes: Seq[String], user: String, pwd: String, databaseName: String):Connection ={
-    if (list_datanodes.isEmpty)
-      throw new Exception("nodelist is mandatory, please provide it.")
-    val available_dns = get_all_active_datanode(list_datanodes, user, pwd, databaseName)
-    val r = Random.nextInt(available_dns.size)
-    val connectionURL = "jdbc:hive2://"+available_dns(r)+":"+sys.env.getOrElse("PORT_IMPALA", "21050")+"/" + databaseName
-    DriverManager.getConnection(connectionURL, user, pwd)
+    val data_node_list = list_datanodes.toBuffer
+    while (data_node_list.nonEmpty){
+      val r = Random.nextInt(data_node_list.size)
+      connection_dn(list_datanodes(r), user, pwd, databaseName) match {
+        case Success(con) => return con
+        case Failure(e) => data_node_list -= list_datanodes(r)
+      }
+    }
+    throw new Exception("No DataNode available")
+  }
+
+  /**
+   * Try to connect to a specific datanode
+   * @param datanode_uri A datanode eg: dn1
+   * @param user          username that has the rights to write into /user/hive/warehouse/
+   * @param pwd           password of the user
+   * @param databaseName  Name of database that user can access
+   */
+
+  def connection_dn(datanode_uri: String, user:String, pwd:String, databaseName: String = "default"):Try[Connection] = {
+    val JDBC_DRIVER_NAME = "org.apache.hive.jdbc.HiveDriver"
+    Class.forName(JDBC_DRIVER_NAME)
+    val connectionURL = "jdbc:hive2://" + datanode_uri + ":" + sys.env.getOrElse("PORT_IMPALA", "21050") + "/" + databaseName
+    Try(DriverManager.getConnection(connectionURL, user, pwd))
   }
 
 
